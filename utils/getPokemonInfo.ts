bf7e7315ade2c125d528ffb4bcf8e7644aae7f48
Camilo Buitrago <camilo.buitrago@workingmouse.com.au>
2022-04-15 22:17:44 +1000

    Incremental render

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,13 +2,18 @@ import { pokeAPI } from "../api";
 import { Pokemon } from "../interfaces";
 
 
-export const getPokemonInfo = async (nameOdId: string) => { 
-	const { data } = await pokeAPI.get<Pokemon>(`/pokemon/${nameOdId}`);
+export const getPokemonInfo = async (nameOdId: string) => {
+	try {
+		const { data } = await pokeAPI.get<Pokemon>(`/pokemon/${nameOdId}`);
 
-	const {id, name: pokemonname, sprites } = data;
-	return {
-		id,
-		name: pokemonname,
-		sprites
+		const { id, name: pokemonname, sprites } = data;
+		return {
+			id,
+			name: pokemonname,
+			sprites
+		}
+	} catch (error) {
+		return null;
 	}
+
 }
